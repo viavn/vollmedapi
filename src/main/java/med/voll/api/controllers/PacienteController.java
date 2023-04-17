c48d3bf9ad54ce8fc52bc6d67b589859390d0744
Vinicius Avansini <vi.avn@hotmail.com>
2023-04-17 17:30:17 -0300

    obtendo paciente por id

--- src/main/java/med/voll/api/controllers/PacienteController.java
+++ src/main/java/med/voll/api/controllers/PacienteController.java
@@ -1,28 +1,32 @@
 package med.voll.api.controllers;
 
 import jakarta.validation.Valid;
-import med.voll.api.dtos.DadosAtualizacaoPaciente;
-import med.voll.api.dtos.DadosCadastroPaciente;
-import med.voll.api.dtos.DadosListagemPaciente;
+import med.voll.api.dtos.*;
 import med.voll.api.entities.Paciente;
+import med.voll.api.exceptions.RecordNotFoundException;
 import med.voll.api.repositories.PacienteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("pacientes")
 public class PacienteController {
-    @Autowired
     private PacienteRepository pacienteRepository;
 
-    @PostMapping
-    @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
-        pacienteRepository.save(new Paciente(dados));
+    public PacienteController(PacienteRepository pacienteRepository) {
+        this.pacienteRepository = pacienteRepository;
+    }
+
+    @GetMapping("/{id}")
+    public DadosDetalhamentoPaciente obtemPorId(@PathVariable("id") Long id) {
+        return pacienteRepository.findById(id).map(DadosDetalhamentoPaciente::new)
+                .orElseThrow(() -> new RecordNotFoundException(id));
     }
 
     @GetMapping
@@ -30,15 +34,25 @@ public class PacienteController {
         return pacienteRepository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new);
     }
 
-    @PutMapping("{id}")
+    @PostMapping
     @Transactional
-    public void atualizar(@PathVariable("id") Long id, @RequestBody @Valid DadosAtualizacaoPaciente dados) {
-        var paciente = pacienteRepository.getReferenceById(id);
-        paciente.atualizarInformacoes(dados);
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroPaciente dados, UriComponentsBuilder uriBuilder) {
+        var paciente = pacienteRepository.save(new Paciente(dados));
+        var uri = uriBuilder.path("/pacientes/{id}").buildAndExpand(paciente.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoPaciente(paciente));
     }
 
-    @DeleteMapping("{id}")
+    @PutMapping("/{id}")
     @Transactional
+    public DadosListagemPaciente atualizar(@PathVariable("id") Long id, @RequestBody @Valid DadosAtualizacaoPaciente dados) {
+        var paciente = pacienteRepository.getReferenceById(id);
+        paciente.atualizarInformacoes(dados);
+        return new DadosListagemPaciente(paciente);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void excluir(@PathVariable("id") Long id) {
         var paciente = pacienteRepository.getReferenceById(id);
         paciente.excluir();
---
+++ src/main/java/med/voll/api/dtos/DadosDetalhamentoPaciente.java
@@ -0,0 +1,10 @@
+package med.voll.api.dtos;
+
+import med.voll.api.entities.Endereco;
+import med.voll.api.entities.Paciente;
+
+public record DadosDetalhamentoPaciente(String nome, String email, String telefone, String cpf, Endereco endereco) {
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(), paciente.getEndereco());
+    }
+}
