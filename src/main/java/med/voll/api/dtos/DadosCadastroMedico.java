4acb06033a545730e2883f84478a4f2b0b67c3c0
Vinicius Avansini <vi.avn@hotmail.com>
2023-04-17 10:18:14 -0300

    adicionando dtos, controller e bd dep

---
+++ src/main/java/med/voll/api/dtos/DadosCadastroMedico.java
@@ -0,0 +1,7 @@
+package med.voll.api.dtos;
+
+import med.voll.api.enums.Especialidade;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade,
+                                  DadosEndereco endereco) {
+}
